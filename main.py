f2a61fbf8abe7b6369fbe122f79f9253c559bfda
Aryann15 <rajlysm15@gmail.com>
2023-07-07 08:59:12 +0530

    create_train function to identify the faces

--- main.py
+++ main.py
@@ -2,19 +2,32 @@ import os
 import cv2 as cv
 import numpy as np
 
-people = ['Robert Downey jr.', 'Chris Evans', 'Chris Hemsworth', 'Jeremy Renner', ' Mark Ruffalo','Scarlett Johansson']
+people = ['Robert downey jr', 'chris evans', 'chris hemsworth', 'jeremy renner', 'mark ruffalo','scarlett johanson']
 DIR = r'C:\Users\rajly\OneDrive\New folder'
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
 
 features = []
 labels = []
 
-def create_label():
+def create_train():
     for person in people:
         path  = os.path.join(DIR, person)
-        label =people.index()
+        label = people.index(person)
 
         for img in os.listdir(path):
             img_path = os.path.join(path,img)
 
             img_array = cv.imread(img_path)
             gray = cv.cvtColor(img_array,cv.COLOR_BGR2GRAY)
+
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6)
+
+            for (x,y,w,h) in faces_rect:
+                faces_roi = gray[y:y+h,x:x+w]
+                features.append(faces_roi)
+                labels.append(label)
+
+create_train()
+
+print(f'Lenfth of the features = {len(features)}')
+print(f'Lenfth of the labels = {len(labels)}')
