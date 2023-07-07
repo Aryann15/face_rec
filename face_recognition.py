bcd0f3f75eb1bb80f9df1ecb052e5fc053a3ec17
Aryann15 <rajlysm15@gmail.com>
2023-07-07 09:35:27 +0530

    face detection

--- face_recognition.py
+++ face_recognition.py
@@ -13,3 +13,13 @@ img= cv.imread(r'C:\Users\rajly\OneDrive\Desktop\validation\evans.jpeg')
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow('Person',gray)
 
+faces_rect = haar_cascade.detectMultiScale(gray,1.1,2)
+for x,y,w,h in faces_rect:
+    faces_roi = gray[y:y+h , x:x+w]
+
+    label,confidence = face_recognizer.predict(faces_roi)
+    print(f' Label= {people[label]} with a confidence of {confidence}')
+
+    cv.putText(img,str(people[label]) , (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0 ,(0,0,255),thickness=2)
+    cv.rectangle(img,(x,y),(x+w,y+h),(0,0,255),thickness=2)
+
